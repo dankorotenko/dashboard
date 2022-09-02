098c9d95075600e317f136fa76e8ce1a4fba87c8
Daniel Korotenko <dankorotenko@Daniels-MacBook-Pro.local>
2022-09-02 18:46:48 +0200

    added calendar and kanban

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,7 +76,7 @@ const Navbar = () => {
             />
             <p>
               <span className='text-gray-400 text-14'>Hi, </span>{''}
-              <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+              <span className='text-gray-400 font-bold ml-1 text-14'>Daniel</span>
             </p>
             <MdKeyboardArrowDown className='text-gray-400 text-14' />
           </div>
